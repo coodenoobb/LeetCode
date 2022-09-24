6d7442a6a5cac95c0588c147c4e677560cf86d5f
Vivek Kumar <88224334+coodenoobb@users.noreply.github.com>
2022-09-24 21:06:31 +0530

    Time: 3 ms (61.25%), Space: 5.8 MB (71.12%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -5,12 +5,10 @@ public:
         if(n==0){
             return false;
         }
-        while(n!=1){
-            if(n%2!=0){
-                return false;     
-            }
-            n=n/2;
+        
+        while(n>1 && (n&1)==0){
+            n=n>>1;
         }
-        return true;
+        return n==1? true:false;
     }
 };
