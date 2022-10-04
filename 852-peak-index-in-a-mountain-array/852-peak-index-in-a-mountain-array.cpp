7eaa1eb091f172582be5798b894bd1847c24b109
Vivek Kumar <88224334+coodenoobb@users.noreply.github.com>
2022-10-04 18:52:25 +0530

    Time: 110 ms (98.89%), Space: 59.7 MB (17.40%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -8,11 +8,11 @@ public:
         int mid = l+((h-l)/2);
         
         while(l<h){
-            if(arr[mid]<arr[mid+1]){
-                l = mid+1;
-            }
-            else{
+            if(arr[mid]>arr[mid+1]){
                 h = mid;
+            }
+            else if (arr[mid]<arr[mid+1]){
+                l = mid + 1;
             }
             mid = l+((h-l)/2);
         }
