3f1aa6e3a0ba8f16fe7fae15d60160059dd6878c
Vivek Kumar <88224334+coodenoobb@users.noreply.github.com>
2022-10-05 13:20:00 +0530

    Time: 5 ms (78.57%), Space: 9.7 MB (22.66%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -11,11 +11,13 @@ public:
             if(arr[mid]==key){
                 return mid;
             }
-            else if(arr[mid]>key){
-                h = mid - 1;
-            }
-            else {
+            
+            else if(arr[mid]<key){
+                
                 l = mid + 1;
+            }
+            else{
+                h = mid - 1;
             }
             mid = l+(h-l)/2;
         }
