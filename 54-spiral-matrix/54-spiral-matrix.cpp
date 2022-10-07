c68e4e084b216ba698101c2541237c3f735885c6
Vivek Kumar <88224334+coodenoobb@users.noreply.github.com>
2022-10-07 18:51:12 +0530

    Time: 0 ms (100.00%), Space: 7 MB (29.09%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -2,47 +2,50 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         
-        int row = matrix.size();
+        vector<int> ans;              // to store new array
+        
+        int row = matrix.size();        
         int col = matrix[0].size();
         
-        int count = 0, n =row*col;
-        vector<int> ans;
+        // Index
         
-        //index
-        int startRow = 0, lastRow = row-1;
-        int startCol = 0, lastCol = col-1;
+        int firstRow = 0, lastRow = row-1;
+        int firstCol = 0, lastCol = col-1;
+        
+        int count = 0, n = row*col;
         
         while(count<n){
             
-        //startRow
-        
-        for(int i = startCol; count<n && i<=lastCol;i++){
-            ans.push_back(matrix[startRow][i]);
-            count++;
-        }   startRow++;
-        
-        //lastCol
-        
-        for(int i= startRow; count<n && i<=lastRow;i++){
-            ans.push_back(matrix[i][lastCol]);
-            count++;
-        }lastCol--;
-        
-        //lastRow
-        
-        for(int i= lastCol; count<n && i>=startCol;i--){
-            ans.push_back(matrix[lastRow][i]);
-            count++;
-        }lastRow--;
-        
-        //startCol
-        
-        for(int i=lastRow; count<n && i>=startRow; i--){
-            ans.push_back(matrix[i][startCol]);
-            count++;
-        }startCol++;
+            // First Row                
             
+            for(int i = firstCol; count<n && i<=lastCol; i++){
+                ans.push_back(matrix[firstRow][i]);
+                count++;
+            }   firstRow++;
+            
+            // Last Col
+            
+            for(int i = firstRow; count<n && i<=lastRow; i++){
+                ans.push_back(matrix[i][lastCol]);
+                count++;
+            }lastCol--;
+            
+            // Last Row
+            
+            for(int i = lastCol; count<n && i>=firstCol; i--){
+                ans.push_back(matrix[lastRow][i]);
+                count++;
+            }lastRow--;
+            
+            // First Col
+            
+            for(int i = lastRow; count<n && i>=firstRow; i--){
+                ans.push_back(matrix[i][firstCol]);
+                count++;
+            }firstCol++;
+                        
         }
-        return ans;
+        
+        return ans;                
     }
 };
