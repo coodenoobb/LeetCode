4c594151a1a1fc533ae39f6370549187c749be78
Vivek Kumar <88224334+coodenoobb@users.noreply.github.com>
2022-10-10 22:28:42 +0530

    Time: 14 ms (5.53%), Space: 12.1 MB (5.75%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        
+        int n = s.length();
+        string ans= "";
+        int count = 0; 
+        
+        for(int i=0;i<n;i++){
+         
+            if(s[i]==' '){
+                count++;
+            }
+            if(count==k){
+                break;
+            }
+            ans = ans+s[i];
+        }
+        return ans;
+    }
+};
