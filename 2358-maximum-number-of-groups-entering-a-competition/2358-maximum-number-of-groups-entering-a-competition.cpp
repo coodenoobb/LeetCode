b67be704640c955d9c448e316cf92c27a7766bf0
Vivek Kumar <88224334+coodenoobb@users.noreply.github.com>
2022-10-13 19:50:10 +0530

    Time: 96 ms (97.34%), Space: 56.5 MB (59.19%) - LeetHub

---
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumGroups(vector<int>& grades) {
+        
+        int n = grades.size();
+        
+        int ans = (-1 + (sqrt(1+8*n)))/2;
+        
+        return ans;
+        
+        
+        
+    }
+};
