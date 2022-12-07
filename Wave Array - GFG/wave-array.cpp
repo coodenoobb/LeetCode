f9e30361d49a48b56beb713270e4d48daecd1e3b
Vivek Kumar <88224334+coodenoobb@users.noreply.github.com>
2022-12-07 18:35:35 +0530

    Added solution - LeetHub

--- Wave Array - GFG/wave-array.cpp
+++ Wave Array - GFG/wave-array.cpp
@@ -13,9 +13,9 @@ class Solution{
     void convertToWave(int n, vector<int>& arr){
         
         // Your code here
-        for(int i=0;i<n-1;){
+        for(int i=0;i<n-1;i+=2){
             swap(arr[i],arr[i+1]);
-            i+=2;
+            
         }
         
     }
